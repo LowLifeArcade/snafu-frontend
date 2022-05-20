5bc6a084ede01526ffb5128253b73d9f5ec087d3
Sonny Brown <sonny.brown@peoplefinders.com>
2022-05-20 08:16:52 -0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="game">
+    <h1>The Game</h1>
+    <input v-model="input" class="shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      ws: null,
+      input: null,
+      user: null,
+    }
+  },
+  mounted() {
+    if (!localStorage.getItem('user')) {
+      this.user = Math.random()
+      console.log('user',this.user)
+      localStorage.setItem('user', this.user)
+    } else {
+      this.user = localStorage.getItem('user')
+    }
+    console.log('user', this.user)
+    this.ws = new WebSocket("ws://localhost:8080")
+    this.ws.addEventListener("open", () => {
+      console.log("We are connected");
+      this.ws.send("How are you?");
+    });
+
+    this.ws.addEventListener("message", function (event) {
+      console.log(event.data);
+    });
+    this.user = localStorage.getItem('user')
+  },
+  watch: {
+    input() {
+      this.ws.send(JSON.stringify({user: this.user, input: this.input}))
+    }
+  }
+};
+</script>
